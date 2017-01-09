1720ca99805808293dd6fad07c583943766c193d
ALI ALRAWI <ali.developer90@gmail.com>
2017-01-09 19:18:18 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function addIngredient(recipe,ing,q) {
+    recipe[ing]=q;
+    return recipe;
+}
+function removeIngredient(recipe,ing,q) {
+  delete recipe[ing];
+  return recipe;
+}
+function updateIngredient(recipe,ing,q) {
+   recipe[ing]=q;
+   return recipe;
+}
+function readRecipe(recipe) {
+  for(var ingredient in recipe)
+  console.log("this recipe calls for "+recipe[ingredient]+" of "+ingredient);
+}
