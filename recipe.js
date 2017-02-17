991cd496ff74e575af64a805de627131aefe7c56
fatmaMY <fatima.mohammad736@gmail.com>
2017-02-18 00:38:34 +0300

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,23 @@
 'use strict';
+function addIngredient(recipe, ingredient, amount) {
+	recipe[ingredient] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient) {
+	delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount) {
+	recipe[ingredient] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+	Object.keys(recipe).forEach(function (key) { 
+    var value = recipe[key]
+ console.log("this recipe calls for " + value + " of " + key);
+});
+
+}
