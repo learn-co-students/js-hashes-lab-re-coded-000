642a5fda2e1095d28484c03afcf98bf5d20e0d74
Mustafa Taha <mtmax1994@gmail.com>
2017-02-25 01:33:58 +0300

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,21 @@
 'use strict';
+function addIngredient(recipe_object,ingredient_name,amounts) {
+	recipe_object[ingredient_name]=amounts;
+	return recipe_object;
+}
+
+function removeIngredient(recipe,ingredient) {
+	delete recipe[ingredient];
+	return recipe;
+}
+
+function updateIngredient(recipe,ingredient,amount) {
+	recipe[ingredient]=amount;
+	return recipe;
+}
+
+function readRecipe(recipe) {
+	for(var key in recipe){
+	console.log("this recipe calls for " + recipe[key] +" of " + key);
+	}
+}
