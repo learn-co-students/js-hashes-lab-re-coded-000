76b247ddbb08e91c76c5a654cca7214a3f777ec7
Ali Diyab <alidiyab1997@gmail.com>
2017-02-25 16:44:54 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function addIngredient(recipe,k,v){
+recipe[k]=v;
+return recipe;
+}
+
+function removeIngredient(recipe,k){
+delete recipe[k];
+return recipe;
+}
+
+function updateIngredient(recipe,k,v){
+  recipe[k]=v;
+  return recipe;
+}
+function readRecipe(recipe){
+  for(var k in recipe)
+    console.log("this recipe calls for " + recipe[k]+ " of " + k);
+}
