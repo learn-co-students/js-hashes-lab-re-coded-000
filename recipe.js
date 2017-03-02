227463394e8a71080869cd9feada55adeae9ddc1
dina aziz <dina.aziz100@yahoo.com>
2017-03-02 21:56:01 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,21 @@
 'use strict';
+function addIngredient(recipe, name, amount){
+  recipe[name]=amount;
+  return recipe
+}
+
+function removeIngredient(recipe,name){
+  delete recipe[name];
+  return recipe
+}
+
+function updateIngredient(recipe, name, amount){
+  recipe[name]=amount;
+  return recipe
+}
+
+function readRecipe(recipe){
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
